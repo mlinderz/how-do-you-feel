b70ea5ea4860978987f512e08f928aacaa0fd9dc
Melinda Santana <melinda.santana@gmail.com>
2019-08-07 20:32:40 -0700

    adding NYtimes
    fixing readme

---
+++ assets/nytimes/nytimes-api.js
@@ -0,0 +1,34 @@
+console.log("loaded")
+var currentMood = ""
+$(".moodButton").on("click", function (){
+    console.log("Mood Changed")
+var newMood =  $(this).val()
+currentMood = newMood
+getArticles(newMood)
+
+})
+var nytAPIkey = "2GYuC8miX6UGJhKCRRCA5GG1y4ct5DMi"
+
+function getArticles (mood){
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ mood +"&api-key=" + nytAPIkey   
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+}).then(function(response){
+    console.log(response)
+var articles = response.response.docs
+for (var i=0; i<articles.length; i++){
+    $("#articles").append(
+        "<p>"+ articles[i].lead_paragraph+ "</p>"
+    )
+}
+
+
+})
+
+}
+
+
+
+
+
