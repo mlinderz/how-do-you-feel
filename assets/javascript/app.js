3ad42704179e9d2ee77a9a0e2e9647fbed59a246
Melinda Santana <melinda.santana@gmail.com>
2019-08-13 19:44:26 -0700

    adding the full paragraph/url
    adding the full paragraph/url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,28 +3,29 @@
 
 //Loading buttons and assigning clicking on a button to change the selection//
 console.log("loaded")
-var currentMood = ""
+var whatToRead = ""
 $(".moodButtonstoRead").on("click", function (){
-    console.log("Mood Changed")
-var newMood =  $(this).val()
-currentMood = newMood
-getArticles(newMood)
+    console.log("You Selected a Button")
+var articleTopic =  $(this).val()
+whatToRead = articleTopic
+getArticles(articleTopic)
 })
 
 
 var nytAPIkey = "2GYuC8miX6UGJhKCRRCA5GG1y4ct5DMi"
 
-function getArticles (mood){
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ mood +"&api-key=" + nytAPIkey   
+function getArticles (whatToRead){
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ whatToRead +"&api-key=" + nytAPIkey   
 $.ajax({
     url: queryURL,
     method: 'GET'
 }).then(function(response){
     console.log(response)
+
 var articles = response.response.docs
 for (var i=0; i<articles.length; i++){
     $("#articles").append(
-        "<p>"+ articles[i].headline.main+ "</p>"
+        "<p>"+ articles[i].headline.main+articles[i].web_url+articles[i].lead_paragraph+ "</p>"
     )
 }
 
