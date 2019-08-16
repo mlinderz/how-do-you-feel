c638328a9e3225b77262f8d51f3e96a7f1e41434
Melinda Santana <melinda.santana@gmail.com>
2019-08-16 08:50:05 -0700

    adding firebase to each js file
    adding firebase to each js file

--- assets/javascript/finish.js
+++ assets/javascript/finish.js
@@ -1,3 +1,28 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Thank you for rebalancing with us! Feel free to rebalance again.";
   }
+
+  //firebase starts here//
+var firebaseConfig = {
+  apiKey: "AIzaSyCj20bQaHGBV-u_tOPHCzt2ECdj3mWnnT4",
+  authDomain: "rebalancing-991cf.firebaseapp.com",
+  databaseURL: "https://rebalancing-991cf.firebaseio.com",
+  projectId: "rebalancing-991cf",
+  storageBucket: "",
+  messagingSenderId: "367645001491",
+  appId: "1:367645001491:web:4fcd1e18a62a8dc0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var newDB = firebase.database();
+
+$('.moodButtons').on('click', function () {
+  var selectedMood = $(this).attr("id");
+  console.log(selectedMood);
+
+  newDB.ref().push({
+    finishedMood: selectedMood
+  });
+});
+
--- assets/javascript/nytimes.js
+++ assets/javascript/nytimes.js
@@ -32,7 +32,31 @@ for (var i=0; i<articles.length; i++){
 })
 
 }
-
+//firebase starts here//
+var firebaseConfig = {
+    apiKey: "AIzaSyCj20bQaHGBV-u_tOPHCzt2ECdj3mWnnT4",
+    authDomain: "rebalancing-991cf.firebaseapp.com",
+    databaseURL: "https://rebalancing-991cf.firebaseio.com",
+    projectId: "rebalancing-991cf",
+    storageBucket: "",
+    messagingSenderId: "367645001491",
+    appId: "1:367645001491:web:4fcd1e18a62a8dc0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  var newDB = firebase.database();
+  
+  $('.moodButtons').on('click', function () {
+    var selectedMood = $(this).attr("id");
+    console.log(selectedMood);
+  
+    newDB.ref().push({
+      firstMoodnytimes: selectedMood
+    });
+  });
+  
+  
     
 
 
